91f551e5bc33c4213a1546452b770a06ee1a2b1c
1207241271 <yangxu@yangxudeMacBook-Pro.local>
2015-09-29 14:35:17 +0900

    Initial Commit

---
+++ School/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  School
+//
+//  Created by yangxu on 15/9/29.
+//  Copyright (c) 2015年 yangxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
